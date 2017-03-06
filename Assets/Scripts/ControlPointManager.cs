13f8d134b8797c28fb50720ab9d38ee01ad6b1b2
Jorge Quero <hangukkhiphop@gmail.com>
2017-03-06 13:41:00 -0600

    Main menu basic implementation Doug joint adjustments Basic game loop: body parts randomly drifting one at a time

--- Assets/Scripts/ControlPointManager.cs
+++ Assets/Scripts/ControlPointManager.cs
@@ -3,27 +3,65 @@ using UnityEngine;
 
 public class ControlPointManager : MonoBehaviour
 {
+    [System.Serializable]
+    public struct Difficulty
+    {
+        public int MaxDriftCount;
+        public float MinDriftDelay;
+        public float MaxDriftDelay;
+    }
+    public Difficulty[] Difficulties;
+
     public ControlPoint[] ControlPoints;
 
-    private int mDriftCount;
-    private int mMaxDriftCount;
+    private bool bRunning;
+    private int mDifficultyLevel;
+    private Difficulty mDifficulty;
 
     private List<ControlPoint> mStableCP;
     private List<ControlPoint> mDriftingCP;
 
-    void Start()
+
+    public void Enable()
     {
+        bRunning = true;
         mStableCP = new List<ControlPoint>(ControlPoints);
         mDriftingCP = new List<ControlPoint>(ControlPoints.Length);
-        
+        IncreaseDifficulty();
     }
 
-    private void SelectNextDrift()
+    public void Disable()
     {
-        int RandomCP = Random.Range(0, mStableCP.Count);
-        ControlPoint ToDriftCP = mStableCP[RandomCP];
+        bRunning = false;
+    }
+
+    void Update()
+    {
+        if (bRunning && mDriftingCP.Count < mDifficulty.MaxDriftCount)
+        {
+            SelectNextDrifter();
+        }
+    }
+
+    public void IncreaseDifficulty()
+    {
+        mDifficulty = Difficulties[mDifficultyLevel];
+        mDifficultyLevel++;
+    }
+
+    private void SelectNextDrifter()
+    {
+        int RandomIndex = Random.Range(0, mStableCP.Count);
+        ControlPoint ToDriftCP = mStableCP[RandomIndex];
         mStableCP.Remove(ToDriftCP);
         mDriftingCP.Add(ToDriftCP);
+        float RandomDelay = Random.Range(mDifficulty.MinDriftDelay, mDifficulty.MaxDriftDelay);
+        ToDriftCP.Destabilize(() => RemoveFromDriftingList(ToDriftCP), RandomDelay);
+    }
 
+    private void RemoveFromDriftingList(ControlPoint ToStableCP)
+    {
+        mDriftingCP.Remove(ToStableCP);
+        mStableCP.Add(ToStableCP);
     }
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    #region Editor
+    
+    public Text PlayOption;
+    public Text HighScoresOption;
+
+    #endregion
+
+
+    #region UI
+
+    public void Play()
+    {
+        GameState.Instance.EnterMainGame();        
+    }
+
+    #endregion
+}
