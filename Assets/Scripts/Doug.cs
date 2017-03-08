fee1270ad58165fce4e2b1206cd82e9d5e47031b
Jorge Quero <hangukkhiphop@gmail.com>
2017-03-07 23:28:59 -0600

    UI framework

---
+++ Assets/Scripts/Doug.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doug : MonoBehaviour
+{
+    public ScoreManager Score;
+
+    private bool bDrinking;
+
+    void Start()
+    {
+        StartDrinking();
+    }
+
+    void Update()
+    {
+        if(bDrinking)
+        {
+            Score.IncrementIntoxication();
+        }
+    }
+
+    public void StartDrinking()
+    {
+        bDrinking = true;
+    }
+
+    public void StopDrinking()
+    {
+        bDrinking = false;
+    }
+
+
+}
