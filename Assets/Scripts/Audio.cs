c86541c1d214a84b52df1dc1b2db18c04b7ec680
Trenton Noble <trentonrnoble@gmail.com>
2017-06-02 18:57:55 -0500

    Audio and audio manager

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audio : MonoBehaviour
+{
+    public void PlaySoundOnce(AudioClip audioClip)
+    {
+        StartCoroutine(PlaySoundCoroutine(audioClip));
+    }
+
+    IEnumerator PlaySoundCoroutine(AudioClip audioClip)
+    {
+        audio.PlayOneShot(audioClip);
+        yield return new WaitForSeconds(audioClip.length);
+        Destroy(gameObject);
+    }
+
+    public void PlaySoundLoop(AudioClip audioClip)
+    {
+        audio.clip = audioClip;
+        audio.loop = true;
+        audio.Play();
+    }
+
+    public void StopSound()
+    {
+        audio.Stop();
+        Destroy(gameObject);
+    }
+}
